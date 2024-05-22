53661ca87b9a0c47782c83acc4ed5209126c8f29
i.karpov <tri3xxx@yandex.ru>
2024-05-22 15:54:51 +0400

    check style

--- src/main/java/org/javaacademy/afisha/repository/TicketRepository.java
+++ src/main/java/org/javaacademy/afisha/repository/TicketRepository.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class TicketRepository {
     private static String SELECT_QUERY = "SELECT * FROM ticket";
-    private static String FIND_QUERY="SELECT * FROM ticket WHERE ID=?";
+    private static String FIND_QUERY = "SELECT * FROM ticket WHERE ID=?";
     private static String INSERT_QUERY = "INSERT INTO ticket(event_id, client_email, price) VALUES(?, ?, ?)";
     private static String UPDATE_QUERY = "UPDATE ticket SET is_sold=? WHERE ID=?";
     @Autowired
@@ -24,7 +24,7 @@ public class TicketRepository {
 
     public List<Ticket> findAll() throws SQLException {
         List<Ticket> tickets = new ArrayList<>();
-        try (Connection connection = dataSource.getConnection()){
+        try (Connection connection = dataSource.getConnection()) {
             try (Statement statement = connection.createStatement()) {
                 ResultSet resultSet = statement.executeQuery(SELECT_QUERY);
                 while (resultSet.next()) {
@@ -39,7 +39,7 @@ public class TicketRepository {
     }
 
     public Optional<Ticket> findById(Long id) throws SQLException {
-        try (Connection connection = dataSource.getConnection()){
+        try (Connection connection = dataSource.getConnection()) {
             try (PreparedStatement preparedStatement = connection.prepareStatement(FIND_QUERY)) {
                 preparedStatement.setLong(1, id);
                 ResultSet resultSet = preparedStatement.executeQuery();
@@ -48,8 +48,8 @@ public class TicketRepository {
                     ticket.setId(resultSet.getLong("id"));
                     return Optional.of(ticket);
                 } else throw new RuntimeException("Не найден билет по ID");
-                }
             }
+        }
     }
 
     public void save(Ticket ticket) {

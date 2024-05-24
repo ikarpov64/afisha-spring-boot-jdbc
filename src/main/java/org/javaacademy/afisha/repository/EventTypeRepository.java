38869da703c650c4ea07b5f5142cb7ad44244ac3
i.karpov <tri3xxx@yandex.ru>
2024-05-24 11:23:09 +0400

    replace connection on jdbc template

--- src/main/java/org/javaacademy/afisha/repository/EventTypeRepository.java
+++ src/main/java/org/javaacademy/afisha/repository/EventTypeRepository.java
@@ -3,11 +3,15 @@ package org.javaacademy.afisha.repository;
 import lombok.RequiredArgsConstructor;
 import org.javaacademy.afisha.entity.EventType;
 import org.javaacademy.afisha.entity.Place;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Component;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Component
@@ -17,50 +21,36 @@ public class EventTypeRepository {
     private static String FIND_QUERY = "SELECT * FROM application.event_type WHERE ID=?";
     private static String INSERT_QUERY = "INSERT INTO application.event_type(name) VALUES(?)";
     private static String UPDATE_QUERY = "UPDATE application.event_type SET is_sold=? WHERE ID=?";
-    private final Connection connection;
+    private final JdbcTemplate jdbcTemplate;
+//    private final Connection connection;
 
-    public List<EventType> findAll() throws SQLException {
-        List<EventType> eventTypes = new ArrayList<>();
-        try (Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(SELECT_QUERY);
-            while (resultSet.next()) {
-                EventType eventType = new EventType();
-                eventType.setId(resultSet.getLong("id"));
-                eventType.setName(resultSet.getString("name"));
-                eventTypes.add(eventType);
-            }
-            return eventTypes;
+    private static final RowMapper<EventType> PLACE_ROW_MAPPER = new RowMapper<EventType>() {
+        @Override
+        public EventType mapRow(ResultSet rs, int rowNum) throws SQLException {
+            EventType eventType = new EventType();
+            eventType.setId(rs.getLong("id"));
+            eventType.setName(rs.getString("name"));
+            return eventType;
         }
+    };
+
+    public List<EventType> findAll() {
+        return jdbcTemplate.query(SELECT_QUERY, PLACE_ROW_MAPPER);
     }
 
-    public Optional<EventType> findById(Long id) throws SQLException {
-        try (PreparedStatement preparedStatement = connection.prepareStatement(FIND_QUERY)) {
-            preparedStatement.setLong(1, id);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
-                    EventType eventType = new EventType();
-                    eventType.setId(resultSet.getLong("id"));
-                    eventType.setName(resultSet.getString("name"));
-                    return Optional.of(eventType);
-                } else {
-                    return Optional.empty();
-                }
-            }
-        }
+    public Optional<EventType> findById(Long id) {
+        List<EventType> eventTypes = jdbcTemplate.query(FIND_QUERY, PLACE_ROW_MAPPER, id);
+        return eventTypes.isEmpty() ? Optional.empty() : Optional.of(eventTypes.get(0));
     }
 
-    public EventType save(EventType eventType) throws SQLException {
-        try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY,
-                Statement.RETURN_GENERATED_KEYS)) {
-            preparedStatement.setString(1, eventType.getName());
-            preparedStatement.executeUpdate();
-
-            try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
-                if (generatedKeys.next()) {
-                    eventType.setId(generatedKeys.getLong(1));
-                }
-            }
-        }
+    public EventType save(EventType eventType) {
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(INSERT_QUERY, new String[]{"id"});
+            ps.setString(1, eventType.getName());
+            return ps;
+        }, keyHolder);
+        eventType.setId(Objects.requireNonNull(keyHolder.getKey()).longValue());
         return eventType;
     }
 }
